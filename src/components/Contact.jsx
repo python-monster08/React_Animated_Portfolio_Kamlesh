2d7ae2454ef4c316cba0e6e336fc41d5b972b333
Kamlesh Lovewanshi <kamleshlovewanshi2000@gmail.com>
2024-05-16 18:48:18 +0530

    Make website Dynamic

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,50 @@
-import React from 'react'
+// import React from 'react'
+// import { FaInstagram } from "react-icons/fa";
+// import { CiFacebook } from "react-icons/ci";
+// import { CiLinkedin } from "react-icons/ci";
+// import { FaXTwitter } from "react-icons/fa6";
+// import { FaGithub } from "react-icons/fa";
+// import { SiGmail } from "react-icons/si";
+// import { IoCall } from "react-icons/io5";
+
+// function Contact() {
+//   return (
+//     <>
+//       <div className="container contact my-5" id='contact'>
+//         <h1>CONTACT ME</h1>
+//         <div className="contact-icon" data-aos="zoom-in-up" data-aos-duration="1000">
+//           <a href='https://www.instagram.com/' target='_blank' className="items">
+//             <FaInstagram className="icons" />
+//           </a>
+//           <a href='https://www.facebook.com/' target='_blank' className="items">
+//             <CiFacebook className="icons" />
+//           </a>
+//           <a href='https://in.linkedin.com/in/kamlesh-lovewanshi' target='_blank' className="items">
+//             <CiLinkedin className="icons" />
+//           </a>
+//           <a href='https://twitter.com/' target='_blank' className="items">
+//             <FaXTwitter className="icons" />
+//           </a>
+//           <a href='https://github.com/python-monster08' target='_blank' className="items">
+//             <FaGithub className="icons" />
+//           </a>
+//           <a href='mailto:kamleshlovewanshi2000@gmail.com' target='_blank' className="items">
+//             <SiGmail className="icons" />
+//           </a>
+//           <a href='tel:+918823857525' className="items">
+//             <IoCall className="icons" />
+//           </a>
+//         </div>
+//       </div>
+//     </>
+//   )
+// }
+
+// export default Contact
+
+
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { FaInstagram } from "react-icons/fa";
 import { CiFacebook } from "react-icons/ci";
 import { CiLinkedin } from "react-icons/ci";
@@ -8,36 +54,51 @@ import { SiGmail } from "react-icons/si";
 import { IoCall } from "react-icons/io5";
 
 function Contact() {
+  const [contactInfo, setContactInfo] = useState(null);
+
+  useEffect(() => {
+    // Fetch the contact data from the backend
+    axios.get('http://127.0.0.1:8000/api/social-media/')
+      .then(response => {
+        setContactInfo(response.data[0]); // Assuming there's only one set of contact info
+      })
+      .catch(error => {
+        console.error('There was an error fetching the contact data!', error);
+      });
+  }, []);
+
   return (
     <>
       <div className="container contact my-5" id='contact'>
         <h1>CONTACT ME</h1>
-        <div className="contact-icon" data-aos="zoom-in-up" data-aos-duration="1000">
-          <a href='https://www.instagram.com/' target='_blank' className="items">
-            <FaInstagram className="icons" />
-          </a>
-          <a href='https://www.facebook.com/' target='_blank' className="items">
-            <CiFacebook className="icons" />
-          </a>
-          <a href='https://in.linkedin.com/in/kamlesh-lovewanshi' target='_blank' className="items">
-            <CiLinkedin className="icons" />
-          </a>
-          <a href='https://twitter.com/' target='_blank' className="items">
-            <FaXTwitter className="icons" />
-          </a>
-          <a href='https://github.com/python-monster08' target='_blank' className="items">
-            <FaGithub className="icons" />
-          </a>
-          <a href='mailto:kamleshlovewanshi2000@gmail.com' target='_blank' className="items">
-            <SiGmail className="icons" />
-          </a>
-          <a href='tel:+918823857525' className="items">
-            <IoCall className="icons" />
-          </a>
-        </div>
+        {contactInfo && (
+          <div className="contact-icon" data-aos="zoom-in-up" data-aos-duration="1000">
+            <a href={contactInfo.instagram_link} target='_blank' className="items" rel="noopener noreferrer">
+              <FaInstagram className="icons" />
+            </a>
+            <a href={contactInfo.facebook_link} target='_blank' className="items" rel="noopener noreferrer">
+              <CiFacebook className="icons" />
+            </a>
+            <a href={contactInfo.linkedin_link} target='_blank' className="items" rel="noopener noreferrer">
+              <CiLinkedin className="icons" />
+            </a>
+            <a href={contactInfo.twitter_link} target='_blank' className="items" rel="noopener noreferrer">
+              <FaXTwitter className="icons" />
+            </a>
+            <a href={contactInfo.github_link} target='_blank' className="items" rel="noopener noreferrer">
+              <FaGithub className="icons" />
+            </a>
+            <a href={`mailto:${contactInfo.mail}`} target='_blank' className="items" rel="noopener noreferrer">
+              <SiGmail className="icons" />
+            </a>
+            <a href={`tel:${contactInfo.contact_no}`} className="items">
+              <IoCall className="icons" />
+            </a>
+          </div>
+        )}
       </div>
     </>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,42 +1,174 @@
-import React, { useEffect, useRef } from 'react'
-import  pdf from '../pdf/resume.pdf'
-import hero from './data/hero.json'
-import Typed from 'typed.js'
+// import React, { useEffect, useRef } from 'react'
+// import  pdf from '../pdf/resume.pdf'
+// import hero from './data/hero.json'
+// import Typed from 'typed.js'
 
 
+
+// function Home() {
+//   const typedRef = useRef(null)
+//   useEffect(() => {
+
+//     const options = {
+//     strings: ["Welcome to My Profile", "My name is Kamlesh Lovewanshi","I'm Python Developer", "Fullstact Developer"],
+//     typeSpeed: 100,
+//     backSpeed: 100,
+//     loop: true,
+//     }
+//     const typed = new Typed(typedRef.current, options)
+//     return() =>{
+//       typed.destroy()
+//     }
+//   }, [])
+//   return (
+//     <>
+//       <div className="container home" id='home'>
+//         <div className=" left" data-aos="fade-up-right" data-aos-duration="1000">
+//           <h1 ref={typedRef}>
+            
+//           </h1>
+//           <a href={pdf} download="Resume.pdf" className="btn btn-outline-warning my-2">Download Resume</a>
+//         </div>
+//         <div className="right">
+//           <div className="img" data-aos="fade-up-left" data-aos-duration="1000">
+//             <img src={`/assets/${hero.imgSrc}`} alt="hero" className="img-fluid" />
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   )
+// }
+
+// export default Home
+
+
+
+// import React, { useEffect, useRef, useState } from 'react';
+// import axios from 'axios';
+// import pdf from '../pdf/resume.pdf';
+// import hero from './data/hero.json';
+// import Typed from 'typed.js';
+
+// function Home() {
+//   const typedRef = useRef(null);
+//   const [typedStrings, setTypedStrings] = useState([]);
+//   const [allStrings, setAllStrings] = useState([]);
+
+//   useEffect(() => {
+//     // Fetch the typed strings from the backend
+//     axios.get('http://127.0.0.1:8000/api/title-strings/')
+//       .then(response => {
+//         setTypedStrings(response.data.strings);
+//         setAllStrings(response.data.strings); // Store all strings for display
+//       })
+//       .catch(error => {
+//         console.error('There was an error fetching the typed strings!', error);
+//       });
+//   }, []);
+
+//   useEffect(() => {
+//     if (typedStrings.length > 0) {
+//       const options = {
+//         strings: typedStrings,
+//         typeSpeed: 100,
+//         backSpeed: 100,
+//         loop: true,
+//       };
+//       const typed = new Typed(typedRef.current, options);
+//       return () => {
+//         typed.destroy();
+//       };
+//     }
+//   }, [typedStrings]);
+
+//   return (
+//     <>
+//       <div className="container home" id='home'>
+//         <div className="left" data-aos="fade-up-right" data-aos-duration="1000">
+//           <h1 ref={typedRef}></h1>
+//           <a href={pdf} download="Resume.pdf" className="btn btn-outline-warning my-2">Download Resume</a>
+//         </div>
+//         <div className="right">
+//           <div className="img" data-aos="fade-up-left" data-aos-duration="1000">
+//             <img src={`/assets/${hero.imgSrc}`} alt="hero" className="img-fluid" />
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default Home;
+
+
+
+import React, { useEffect, useRef, useState } from 'react';
+import axios from 'axios';
+import Typed from 'typed.js';
 
 function Home() {
-  const typedRef = useRef(null)
-  useEffect(() => {
+  const typedRef = useRef(null);
+  const [typedStrings, setTypedStrings] = useState([]);
+  const [allStrings, setAllStrings] = useState([]);
+  const [profile, setProfile] = useState(null);
 
-    const options = {
-    strings: ["Welcome to My Profile", "My name is Kamlesh Lovewanshi","I'm Python Developer", "Fullstact Developer"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    loop: true,
+  useEffect(() => {
+    // Fetch the typed strings from the backend
+    axios.get('http://127.0.0.1:8000/api/title-strings/')
+      .then(response => {
+        setTypedStrings(response.data.strings);
+        setAllStrings(response.data.strings); // Store all strings for display
+      })
+      .catch(error => {
+        console.error('There was an error fetching the typed strings!', error);
+      });
+
+    // Fetch the profile data from the backend
+    axios.get('http://127.0.0.1:8000/api/profiles/1/') // Change the id as needed
+      .then(response => {
+        setProfile(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the profile data!', error);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (typedStrings.length > 0) {
+      const options = {
+        strings: typedStrings,
+        typeSpeed: 100,
+        backSpeed: 100,
+        loop: true,
+      };
+      const typed = new Typed(typedRef.current, options);
+      return () => {
+        typed.destroy();
+      };
     }
-    const typed = new Typed(typedRef.current, options)
-    return() =>{
-      typed.destroy()
-    }
-  }, [])
+  }, [typedStrings]);
+
   return (
     <>
       <div className="container home" id='home'>
-        <div className=" left" data-aos="fade-up-right" data-aos-duration="1000">
-          <h1 ref={typedRef}>
-            
-          </h1>
-          <a href={pdf} download="Resume.pdf" className="btn btn-outline-warning my-2">Download Resume</a>
+        <div className="left" data-aos="fade-up-right" data-aos-duration="1000">
+          <h1 ref={typedRef}></h1>
+          {profile && (
+            <>
+              <a href={profile.resume} download="Resume.pdf" className="btn btn-outline-warning my-2">Download Resume</a>
+            </>
+          )}
         </div>
         <div className="right">
           <div className="img" data-aos="fade-up-left" data-aos-duration="1000">
-            <img src={`/assets/${hero.imgSrc}`} alt="hero" className="img-fluid" />
+            {profile && (
+              <img src={profile.imageSrc} alt="hero" className="img-fluid" />
+            )}
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
