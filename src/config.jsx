6c75591283c2798c18628ffd7e707ff07942bd38
Kamlesh Lovewanshi <kamleshlovewanshi2000@gmail.com>
2024-05-20 17:07:50 +0530

    update config.js file

--- src/config.jsx
+++ src/config.jsx
@@ -1,3 +1,3 @@
 // ../config.js
-export const API_BASE_URL = 'http://127.0.0.1:8000/api/';
-// export const API_BASE_URL = 'https://kamlesh-lovewanshi-portfolio.onrender.com/api/';
+// export const API_BASE_URL = 'http://127.0.0.1:8000/api/';
+export const API_BASE_URL = 'https://kamlesh-lovewanshi-portfolio.onrender.com/api/';
