11b10ee1c104789b6cb371ea6bf6caffadab1def
Kamlesh Lovewanshi <kamleshlovewanshi2000@gmail.com>
2024-05-21 15:12:23 +0530

    add env variable

--- src/config.jsx
+++ src/config.jsx
@@ -1,3 +1,6 @@
 // ../config.js
 // export const API_BASE_URL = 'http://127.0.0.1:8000/api/';
-export const API_BASE_URL = 'https://kamlesh-lovewanshi-portfolio.onrender.com/api/';
+// export const API_BASE_URL = 'https://kamlesh-lovewanshi-portfolio.onrender.com/api/';
+// config.js
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+
