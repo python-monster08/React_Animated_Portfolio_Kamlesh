198fa9162c8c367cd7a3041312505a6ca4a423f3
Kamlesh Lovewanshi <kamleshlovewanshi2000@gmail.com>
2024-05-21 16:02:06 +0530

    remove .env file

--- src/config.jsx
+++ src/config.jsx
@@ -1,8 +1,5 @@
 // ../config.js
 // export const API_BASE_URL = 'http://127.0.0.1:8000/api/';
-// export const API_BASE_URL = 'https://kamlesh-lovewanshi-portfolio.onrender.com/api/';
-
-// config.js
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+export const API_BASE_URL = 'https://kamlesh-lovewanshi-portfolio.onrender.com/api/';
 
 
